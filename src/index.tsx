209a572edc31f5a78141b8af5315b78ee66e07b2
Hikaru Kimura <>
2026-06-05 10:36:57 +0900

    alpine が効いてない

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import { serveStatic } from "hono/bun";
 import app from "./app";
 
-app.get("/static/htmx.min.js", serveStatic({ path: "./public/static/htmx.min.js" }));
-app.get("/static/alpine.min.js", serveStatic({ path: "./public/static/cdn.min.js" }));
-app.get("/static/pico.min.css", serveStatic({ path: "./public/static/pico.min.css" }));
+app.get(
+  "/static/htmx.min.js",
+  serveStatic({ path: "./public/static/htmx.min.js" })
+);
+app.get(
+  "/static/alpine.min.js",
+  serveStatic({ path: "./public/static/alpine.min.js" })
+);
+app.get(
+  "/static/pico.min.css",
+  serveStatic({ path: "./public/static/pico.min.css" })
+);
 app.get("/static/app.css", serveStatic({ path: "./public/static/app.css" }));
-app.get("/static/favicon.svg", serveStatic({ path: "./public/static/favicon.svg" }));
+app.get(
+  "/static/favicon.svg",
+  serveStatic({ path: "./public/static/favicon.svg" })
+);
 app.get("/favicon.ico", serveStatic({ path: "./public/favicon.ico" }));
 
 export default {
